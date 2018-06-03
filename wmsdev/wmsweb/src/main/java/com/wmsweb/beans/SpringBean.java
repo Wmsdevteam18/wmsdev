36348a3742b870c3810eb8bac1c05a8728615678
Pavan <swysoft@gmail.com>
2018-06-03 19:08:52 +0530

    Changing package names of wmsweb from org.* to com.* and autowire testing
    package nane changes and added code to test autowire, user /wmsweb/test
    to test autowiring

---
+++ wmsdev/wmsweb/src/main/java/com/wmsweb/beans/SpringBean.java
@@ -0,0 +1,12 @@
+package com.wmsweb.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBean {
+	
+	public String getMessage() {
+		return "Spring bean message";
+	}
+
+}
--- wmsdev/wmsweb/src/main/java/com/wmsweb/controller/HomeController.java
+++ wmsdev/wmsweb/src/main/java/com/wmsweb/controller/HomeController.java
@@ -1,18 +1,30 @@
-package org.wmsweb.controller;
+package com.wmsweb.controller;
 
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.wmsweb.beans.SpringBean;
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	SpringBean bean;
 
 	@RequestMapping(value="/")
 	public ModelAndView test(HttpServletResponse response) throws IOException{
 		return new ModelAndView("home");
+	}
+	
+	@RequestMapping(value="/test")
+	public @ResponseBody String testMessage() {
+		return bean.getMessage();
 	}
 }
