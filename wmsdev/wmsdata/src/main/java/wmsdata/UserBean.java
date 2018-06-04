683e96dace37aa4950a42b2adfc59c721e29f797
Pavan <swysoft@gmail.com>
2018-06-04 23:05:55 +0530

    Adding jpa2 api to wmsdata and userbean annotated with @entity

--- wmsdev/wmsdata/src/main/java/wmsdata/UserBean.java
+++ wmsdev/wmsdata/src/main/java/wmsdata/UserBean.java
@@ -2,11 +2,19 @@ package wmsdata;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
+@Entity
+@Table(name="password")
 public class UserBean implements Serializable{
 	
+	@GeneratedValue
+	@Id
 	private String userName;
 	private String password;
 	
